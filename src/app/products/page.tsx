336403940f9a6c45de2bf5d19b18d6f277fda741
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-09-09 16:47:39 +0200

    add subpage with products

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { ProductList } from "@/ui/organisms/ProductList";
 import { type ProductItemType } from "@/ui/types";
 
@@ -48,8 +47,7 @@ const products: ProductItemType[] = [
 export default function Home() {
 	return (
 		<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
-			{/* <ProductList products={products} /> */}
-			<Link href="/products">Zobacz liste naszych produktów</Link>
+			<ProductList products={products} />
 		</section>
 	);
 }
