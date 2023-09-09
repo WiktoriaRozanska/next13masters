a3821a804fe38ae42cfe40dd2a088ab011d4bbfe
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-09-09 20:21:13 +0200

    add subpages: Terms and conditions & Privacy Policy

---
+++ src/app/(static-pages)/regulamin/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+	return (
+		<>
+			<h1 className="text-3xl font-bold">Regulamin</h1>
+			<p className="text-xl font-bold">§1</p>
+			<p className="text-xl font-bold">§2</p>
+			<p className="text-xl font-bold">§3</p>
+		</>
+	);
+}
---
+++ src/app/polityka-prywatnosci/page.tsx
@@ -0,0 +1,10 @@
+export default function PolitykaPrywatnosci() {
+	return (
+		<div className="mx-auto max-w-md text-center">
+			<h1 className="text-3xl font-bold">Polityka prywatności</h1>
+			<p className="text-xl font-bold">§1</p>
+			<p className="text-xl font-bold">§2</p>
+			<p className="text-xl font-bold">§3</p>
+		</div>
+	);
+}
