cfa6cde54a7b32fd2252edd8330fc544a0e1ddfc
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-09-17 06:54:45 +0200

    2.6: Link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ActiveLink } from "@/ui/atoms/ActiveLink";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang="pl">
 			<body className={inter.className}>
+				<nav>
+					<ul className="mt-2 flex justify-center space-x-4">
+						<li>
+							<ActiveLink href="/">Homepage</ActiveLink>
+						</li>
+						<li>
+							<ActiveLink href="/products">Products</ActiveLink>
+						</li>
+					</ul>
+				</nav>
 				<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
 					{children}
 				</section>
