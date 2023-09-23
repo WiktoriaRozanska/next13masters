0bde8614b892d3e27189400dbaf84dbb5af7c33b
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-09-23 12:29:19 +0200

    3.5

--- src/api/products.ts
+++ src/api/products.ts
@@ -16,13 +16,71 @@ type Rating = {
 	count: number;
 };
 
-export const getProductsList = async (take = 20, offset = 0) => {
-	const res = await fetch(`https://naszsklep-api.vercel.app/api/products?take=${take}&offset=${offset}`);
-	// const products = (await res.json()) as ProductItemType[]; // dokonujemy typowania danych
-	const productsResponse = (await res.json()) as ProductResponseItem[];
-	const products = productsResponse.map(productResponseItemToProductItemType);
-	// const products = productsResponse.map((product) => productResponseItemToProductItemType(product));
-	return products;
+export const getProductsList = async (take = 20, offset = 0): Promise<ProductItemType[]> => {
+	// const res = await fetch(`https://naszsklep-api.vercel.app/api/products?take=${take}&offset=${offset}`);
+	// // const products = (await res.json()) as ProductItemType[]; // dokonujemy typowania danych
+	// const productsResponse = (await res.json()) as ProductResponseItem[];
+	// const products = productsResponse.map(productResponseItemToProductItemType);
+	// // const products = productsResponse.map((product) => productResponseItemToProductItemType(product));
+	// return products;
+	const res = await fetch(
+		"https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clihaom3j03ep01te1dg24yp5/master",
+		{
+			method: "POST",
+			body: JSON.stringify({
+				query: /* GraphQL */ `
+					query GetProductsList {
+						products(first: 10) {
+							id
+							name
+							description
+							images {
+								url
+							}
+							price
+						}
+					}
+				`,
+			}),
+			headers: { "Content-Type": "application/json" },
+		},
+	);
+
+	type GraphQLResponse<T> =
+		| { data?: undefined; errors: { message: string }[] }
+		| { data: T; errors?: undefined };
+
+	type ProductsGraphqlResponse = {
+		products: {
+			id: string;
+			name: string;
+			description: string;
+			images: {
+				url: string;
+			}[];
+			price: number;
+		}[];
+	};
+
+	const graphqlResponse = (await res.json()) as GraphQLResponse<ProductsGraphqlResponse>;
+
+	if (graphqlResponse.errors) {
+		throw TypeError(graphqlResponse.errors[0].message);
+	}
+
+	return graphqlResponse.data.products.map((product) => {
+		return {
+			id: product.id,
+			category: "",
+			name: product.name,
+			price: product.price,
+			description: product.description,
+			coverImage: {
+				src: product.images[0].url,
+				alt: product.name,
+			},
+		};
+	});
 };
 
 export const getNumberOfProducts = async () => {
