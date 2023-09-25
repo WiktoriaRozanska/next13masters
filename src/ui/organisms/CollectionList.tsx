22c11b04f41577deffd182f005b2821135baba57
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-09-25 22:33:01 +0200

    add data-testId

--- src/ui/organisms/CollectionList.tsx
+++ src/ui/organisms/CollectionList.tsx
@@ -3,10 +3,7 @@ import { CollectionListItem } from "@/ui/molecules/CollectionListItem";
 
 export const CollectionList = ({ collections }: { collections: CollectionListItemFragment[] }) => {
 	return (
-		<ul
-			className="m:grid smd:grid-cols-2 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3"
-			data-testid="products-list"
-		>
+		<ul className="m:grid smd:grid-cols-2 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
 			{collections.map((collection) => {
 				return <CollectionListItem key={collection.id} collection={collection} />;
 			})}
--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -5,7 +5,7 @@ export const ProductList = ({ products }: { products: ProductListItemFragment[] 
 	return (
 		<ul
 			className="m:grid smd:grid-cols-3 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4"
-			data-testid="related-products"
+			data-testid="products-list"
 		>
 			{products.map((product) => {
 				return <ProductListItem key={product.id} product={product} />;
