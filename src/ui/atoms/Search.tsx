98896355104523f6d78d7db52b039179c4edcd64
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-09-26 20:15:23 +0200

    remove svg

--- src/ui/atoms/Search.tsx
+++ src/ui/atoms/Search.tsx
@@ -45,21 +45,12 @@ export const Search = (props: SearchProps) => {
 					type="search"
 					name="search"
 					placeholder="Enter search..."
-					className="h-10 w-full rounded-full bg-white px-5 pr-10 text-sm focus:outline-none"
+					className="h-10 w-full rounded-full border-black bg-white px-5 pr-10 text-sm focus:outline-none"
 					value={value}
 					onChange={searchHandler}
 					onKeyDown={handleKeyDown}
 					role="searchbox"
 				/>
-				<button type="submit" className="absolute right-0 top-0 mr-4 mt-3">
-					<svg className="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-						<path
-							fillRule="evenodd"
-							d="M13.53 14.47a8 8 0 111.414-1.414l3.96 3.96a1 1 0 01-1.414 1.414l-3.96-3.96zM8 14a6 6 0 100-12 6 a 6 0 000 12z"
-							clipRule="evenodd"
-						/>
-					</svg>
-				</button>
 			</div>
 		</form>
 	);
