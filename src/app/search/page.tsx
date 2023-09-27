b15e299d24f6ec47d1bc37937ab731d29a552a0f
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-09-27 12:48:26 +0200

    fix search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,19 +1,25 @@
 "use client";
 
 import { getProductsByProvidedValue } from "@/api/products";
+import { ProductListItemFragment } from "@/gql/graphql";
 import { ProductList } from "@/ui/organisms/ProductList";
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default async function SearchPage() {
+export default function SearchPage() {
 	const searchParams = useSearchParams();
 	const searchValue = searchParams.get("query");
-	const products = await getProductsByProvidedValue(searchValue || "");
+
+	const [data, setData] = useState([] as ProductListItemFragment[]);
+	useEffect(() => {
+		getProductsByProvidedValue(searchValue || "").then((data) => setData(data));
+	});
 
 	return (
 		<>
 			<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
-				{!products.length && <h1>No products found that meet your criteria</h1>}
-				<ProductList products={products} />
+				{!data.length && <h1>No products found that meet your criteria</h1>}
+				<ProductList products={data} />
 			</section>
 		</>
 	);
